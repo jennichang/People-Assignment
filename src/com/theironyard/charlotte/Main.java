96634bca83bfa902fa308f83fdb5c71c7992ca42
Jennifer Chang <jennichang02@gmail.com>
2016-08-13 13:25:37 -0400

    updated assignment including arraylist sort within hashamp

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -7,8 +7,7 @@ import java.util.*;
 public class Main {
 
     public static HashMap<String, ArrayList<Person>> map = new HashMap<>();
-    public static ArrayList<Person> personArrayList = new ArrayList<>();
-
+    
     public static void main(String[] args) throws FileNotFoundException {
 
         // read all the people into memory
@@ -19,23 +18,21 @@ public class Main {
             String[] columns = line.split(","); // since our different fields are deliminated by comma
             // make array list (of 6 strings) by that split
             Person personObject = new Person(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3],
-                    columns[4], columns[5]); // why won't int for id work?, b/c the first line were the headers
+                    columns[4], columns[5]); // why won't int for id work?, b/c the first line were the headers.
+            //had to delete header from txt.  need to figure out how to fix without deleting.
 
             //country in as hashmap key with empty arraylist as value
-            map.putIfAbsent(columns[4], new ArrayList<>());
+            map.putIfAbsent(columns[4], new ArrayList<>()); //if the country is not already in the hashmap, add it and as
+            //its value, put an empty arraylist.
 
-            map.get(columns[4]).add(personObject);
-
-        }
-    }
-
-//        // for each hash map key, sort the array list by last name
-
-    public static void sortLastName() {
-        for (int i = 0; i < map.size(); i++) {
-
+            map.get(columns[4]).add((personObject)); //not too sure how add knows to add based on country though?
 
         }
+        for(String key:map.keySet()){  // for every key in the hashamp
+            Collections.sort(map.get(key)); //get the value at the key, and sort
+            System.out.println("[KEY: "+key+" , People: "+map.get(key)); // print out
+        } //IT WORKS. IT WORKS. IT WORKS.
+
     }
 }
 
@@ -44,73 +41,27 @@ public class Main {
 
 
 
+/* Note:
+     for each element in array list currently have, look at person, look at country, use country as a key in hash
+     add person to list of people at the hashmap.
 
+     putifabsent a new array list on specific key.
+     putifabset new array list at
 
+     for each read in a person, call putifabset and key would be that person's country and value
+     would be new array list (empty)
+     next line, hashmap.get(persons country), that will get me a new array list, use add method,
+     hasmap.get.person.country.add
 
-
-
-
-        //WHILE LOOP & ITERATOR
-//        Iterator iterator = map.entrySet().iterator();
-//        while (iterator.hasNext()) {
-//            Map.Entry me2 = (Map.Entry) iterator.next();
-//            Collections.sort(personArrayList);
-//            System.out.println("Key: " + me2.getKey() + me2.getValue());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //for each element in array list currently have, look at person, look at country, use country as a key in hash
-        // add person to list of people at the hashmap.
-
-        //putifabsent a new array list on specific key.
-        //putifabset new array list at
-
-        // for each read in a person, call putifabset and key would be that person's country and value
-        // would be new array list (empty)
-        //next line, hashmap.get(persons country), that will get me a new array list, use add method,
-        // hasmap.get.person.country.add
-
-
-            /*
             hashmap = data
 
             data.get() -- return object at key
             data.get(country) -- get will return value, that will bring back array list
             data.get(country.add(personObject)
 
+      System.out.println(personArrayList);
+      System.out.println(map);
 
+map.putIfAbsent(columns[4], personArrayList );
 
-
-             */
-
-
-        //System.out.println(personArrayList);
-        //System.out.println(map);
-
-//            map.putIfAbsent(columns[4], personArrayList );
-//
-//            System.out.println(map);
-
-//retrieve arraylist at the hashmap entry...hashmap.get(country) that gives you arraylist and you add to that
-
-        //hashmap.get.add(newperson)  and it will add new person
-
-
+*/
